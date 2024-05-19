73be8630c67f18a0f4f67367319ab616491536f0
byakuren-hijiri <159621697+byakuren-hijiri@users.noreply.github.com>
2024-05-19 10:30:08 -0300

    Save references to AST positions in facts (#15)
    Rewrite the `Souffle` library and the corresponding logic to support mappings to AST positions. Additionally, this adds the updated `readOnlyVariables` lint to showcase the updated API.

--- src/detectors/builtin/readOnlyVariables.ts
+++ src/detectors/builtin/readOnlyVariables.ts
@@ -1,7 +1,18 @@
+import { ASTStatement, ASTRef } from "@tact-lang/compiler/dist/grammar/ast";
 import { Detector } from "../detector";
 import { MistiContext } from "../../internals/context";
-import { CompilationUnit } from "../../internals/ir";
-import { MistiTactError } from "../../internals/errors";
+import { CompilationUnit, Node, CFG } from "../../internals/ir";
+import {
+  Context,
+  Fact,
+  FactType,
+  Relation,
+  Executor,
+  Rule,
+  RuleBody,
+  Atom,
+} from "../../internals/souffle";
+import { MistiTactError, Severity } from "../../internals/errors";
 
 /**
  * A detector that identifies read-only variables and fields.
@@ -14,8 +25,150 @@ export class ReadOnlyVariables extends Detector {
     return "ROV";
   }
 
-  check(ctx: MistiContext, _cu: CompilationUnit): MistiTactError[] {
-    ctx.logger.info("Checking for read-only variables...");
-    return []; // TODO
+  check(ctx: MistiContext, cu: CompilationUnit): MistiTactError[] {
+    ctx.logger.debug("Checking for read-only variables...");
+
+    const program = new Context<ASTRef>(this.id);
+    this.addDecls(program);
+    this.addRules(program);
+    this.addConstraints(cu, program);
+
+    const executor = ctx.config.soufflePath
+      ? new Executor<ASTRef>({
+          inputDir: ctx.config.soufflePath,
+          outputDir: ctx.config.soufflePath,
+        })
+      : new Executor<ASTRef>();
+    const result = executor.executeSync(program);
+    if (!result.success) {
+      throw new Error(`Error executing Soufflé: ${result.stderr}`);
+    }
+
+    const warnings = Array.from(result.results.entries.values()).map((fact) => {
+      if (fact.data === undefined) {
+        throw new Error(`AST position for fact ${fact} is not available`);
+      }
+      return this.createError(
+        "Variable is never used",
+        Severity.MEDIUM,
+        fact.data,
+      );
+    });
+
+    return warnings;
+  }
+
+  /**
+   * Adds declarations to the Souffle program to represent the properties of variables.
+   * @param ctx The Souffle program where the relations are to be added.
+   */
+  addDecls(ctx: Context<ASTRef>) {
+    ctx.add(
+      Relation.from(
+        "varDecl",
+        [
+          ["var", FactType.Symbol],
+          ["func", FactType.Symbol],
+        ],
+        undefined,
+      ),
+    );
+    ctx.add(
+      Relation.from(
+        "varAssign",
+        [
+          ["var", FactType.Symbol],
+          ["func", FactType.Symbol],
+        ],
+        undefined,
+      ),
+    );
+    ctx.add(
+      Relation.from(
+        "varUse",
+        [
+          ["var", FactType.Symbol],
+          ["func", FactType.Symbol],
+        ],
+        undefined,
+      ),
+    );
+    ctx.add(
+      Relation.from(
+        "readOnly",
+        [
+          ["var", FactType.Symbol],
+          ["func", FactType.Symbol],
+        ],
+        "output",
+      ),
+    );
+  }
+
+  /**
+   * Collects facts based on the IR to populate the Souffle program.
+   * @param cu The compilation unit containing the CFGs and AST information.
+   * @param ctx The Souffle program to which the facts are added.
+   */
+  addConstraints(cu: CompilationUnit, ctx: Context<ASTRef>) {
+    cu.forEachCFG(cu.ast, (cfg: CFG, _: Node, stmt: ASTStatement) => {
+      if (cfg.origin === "stdlib") {
+        return;
+      }
+      const funName = cfg.name;
+      switch (stmt.kind) {
+        case "statement_let":
+          ctx.addFact("varDecl", Fact.from([stmt.name, funName], stmt.ref));
+          break;
+        case "statement_assign":
+        case "statement_augmentedassign":
+          // NOTE: Variables unpacking is not supported
+          ctx.addFact(
+            "varAssign",
+            Fact.from([stmt.path[0].name, funName], stmt.ref),
+          );
+          break;
+        case "statement_expression":
+          // TODO: Other cases?
+          if (stmt.expression.kind === "id") {
+            ctx.addFact(
+              "varUse",
+              Fact.from([stmt.expression.value, funName], stmt.ref),
+            );
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  addRules(ctx: Context<ASTRef>) {
+    // readOnly(var, func) :-
+    //     varDecl(var, func),
+    //     varAssign(var, func),
+    //     !varUse(var, func).
+    ctx.add(
+      Rule.from(
+        [Atom.from("readOnly", ["var", "func"])],
+        RuleBody.from(Atom.from("varDecl", ["var", "func"])),
+        RuleBody.from(Atom.from("varAssign", ["var", "func"])),
+        RuleBody.from(Atom.from("varUse", ["var", "func"]), { negated: true }),
+      ),
+    );
+    // readOnly(var, func) :-
+    //     varDecl(var, func),
+    //     !varAssign(var, func),
+    //     !varUse(var, func).
+    ctx.add(
+      Rule.from(
+        [Atom.from("readOnly", ["var", "func"])],
+        RuleBody.from(Atom.from("varDecl", ["var", "func"])),
+        RuleBody.from(Atom.from("varAssign", ["var", "func"]), {
+          negated: true,
+        }),
+        RuleBody.from(Atom.from("varUse", ["var", "func"]), { negated: true }),
+      ),
+    );
   }
 }
---
+++ src/internals/souffle/index.ts
@@ -0,0 +1,10 @@
+export { Fact, FactValue, FactType } from "./fact";
+export {
+  RelationName,
+  RelationArgName,
+  RelationIO,
+  Relation,
+} from "./relation";
+export { Atom, RuleHead, RuleName, RuleBody, Rule } from "./rule";
+export { Context } from "./context";
+export { Executor } from "./executor";
