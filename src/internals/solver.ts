37d280af470882f91045630ff487ef78fd1ae996
Byakuren Hijiri <seclusion5873@proton.me>
2024-06-24 04:06:04 +0000

    chore(solver): Use node indexes instead of nodes

--- src/internals/solver.ts
+++ src/internals/solver.ts
@@ -1,26 +1,26 @@
 import { JoinSemilattice } from "./lattice";
-import { CFG, Node } from "./ir";
+import { CFG, Node, NodeIdx } from "./ir";
 
 /**
  * Results of solving a generic dataflow problem.
  * @template State The type representing the state in the dataflow analysis.
  */
 export class SolverResults<State> {
-  private stateMap: Map<Node, State>;
+  private stateMap: Map<NodeIdx, State>;
 
   constructor() {
     this.stateMap = new Map();
   }
 
-  public getState(node: Node): State | undefined {
-    return this.stateMap.get(node);
+  public getState(idx: NodeIdx): State | undefined {
+    return this.stateMap.get(idx);
   }
 
-  public setState(node: Node, state: State): void {
-    this.stateMap.set(node, state);
+  public setState(idx: NodeIdx, state: State): void {
+    this.stateMap.set(idx, state);
   }
 
-  public getStates(): Map<Node, State> {
+  public getStates(): Map<NodeIdx, State> {
     return this.stateMap;
   }
 }
@@ -68,19 +68,19 @@ export class Solver<State> {
 
     const nodes: Node[] = this.cfg.nodes;
     nodes.forEach((node) => {
-      results.setState(node, this.lattice.bottom());
+      results.setState(node.idx, this.lattice.bottom());
     });
 
     while (worklist.length > 0) {
       const node = worklist.pop()!;
       const inState = this.getPredecessors(node).reduce((acc, pred) => {
-        return this.lattice.join(acc, results.getState(pred)!);
+        return this.lattice.join(acc, results.getState(pred.idx)!);
       }, this.lattice.bottom());
 
       const outState = this.transfer(node, inState);
 
-      if (!this.lattice.leq(outState, results.getState(node)!)) {
-        results.setState(node, outState);
+      if (!this.lattice.leq(outState, results.getState(node.idx)!)) {
+        results.setState(node.idx, outState);
         worklist.push(...this.getPredecessors(node));
       }
     }
