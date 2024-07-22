38009512724fb246eefe75b497f7edfa571bf190
Byakuren Hijiri <seclusion5873@proton.me>
2024-07-22 07:29:27 +0000

    feat(lints): Add extended docstrings with bad/good examples

--- src/detectors/builtin/readOnlyVariables.ts
+++ src/detectors/builtin/readOnlyVariables.ts
@@ -22,8 +22,28 @@ import { forEachExpression } from "../../internals/tactASTUtil";
 /**
  * A detector that identifies read-only variables and fields.
  *
+ * ## Why is it bad?
  * These variables could typically be replaced with constants to optimize performance.
  * Alternatively, identifying read-only variables may reveal issues where unused values are being replaced unintentionally.
+ *
+ * ## Example
+ * ```tact
+ * fun calculateFinalPrice(price: Int): Int {
+ *   // Warning: the developer uses a read-only variable that could be a constant
+ *   let DISCOUNT_AMOUNT: Int = 10;
+ *   return price - DISCOUNT_AMOUNT;
+ * }
+ * ```
+ *
+ * Use instead:
+ * ```tact
+ * const DISCOUNT_AMOUNT: Int = 10;
+ *
+ * fun calculateFinalPrice(price: Int): Int {
+ *   // OK: Fixed after the linter highlighted this warning
+ *   return price - DISCOUNT_AMOUNT;
+ * }
+ * ```
  */
 export class ReadOnlyVariables extends Detector {
   check(ctx: MistiContext, cu: CompilationUnit): MistiTactError[] {
--- src/detectors/builtin/zeroAddress.ts
+++ src/detectors/builtin/zeroAddress.ts
@@ -25,12 +25,41 @@ function findZeroAddress(
 }
 
 /**
- * A detector that identifies uses of zero address.
+ * A detector that identifies uses of the zero address.
  *
+ * ## Why is it bad?
  * Using the zero address in smart contracts is typically problematic because it can be
  * exploited as a default or uninitialized address, leading to unintended transfers and
  * security vulnerabilities. Additionally, operations involving the zero address can
  * result in loss of funds or tokens, as there is no private key to access this address.
+ *
+ * ## Example
+ * ```tact
+ * contract Proxy {
+ *   to: Address;
+ *   init() {
+ *     // Warning: Insecure usage of zero address as default value
+ *     self.to = newAddress(0, 0);
+ *   }
+ *   fun setAddress(to: Address) {
+ *     self.to = to
+ *   }
+ * }
+ * ```
+ *
+ * Use instead:
+ * ```tact
+ * contract Proxy {
+ *   to: Address;
+ *   init(to: Address) {
+ *     // Fixed: Using the input value on initializaiton.
+ *     self.to = to;
+ *   }
+ *   fun setAddress(to: Address) {
+ *     self.to = to
+ *   }
+ * }
+ * ```
  */
 export class ZeroAddress extends Detector {
   check(_ctx: MistiContext, cu: CompilationUnit): MistiTactError[] {
