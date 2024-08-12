cc9a2bd55f80e1fb73a0a49cc1a14777fcbb3fe5
Georgiy Komarov <jubnzv@gmail.com>
2024-08-12 10:24:05 +0000

    chore(zeroAddress): Comment position

--- src/detectors/builtin/zeroAddress.ts
+++ src/detectors/builtin/zeroAddress.ts
@@ -4,44 +4,44 @@ import { makeDocURL, MistiTactError, Severity } from "../../internals/errors";
 import { foldExpressions } from "../../internals/tactASTUtil";
 import { AstExpression } from "@tact-lang/compiler/dist/grammar/ast";
 
+/**
+ * A detector that identifies uses of the zero address.
+ *
+ * ## Why is it bad?
+ * Using the zero address in smart contracts is typically problematic because it can be
+ * exploited as a default or uninitialized address, leading to unintended transfers and
+ * security vulnerabilities. Additionally, operations involving the zero address can
+ * result in loss of funds or tokens, as there is no private key to access this address.
+ *
+ * ## Example
+ * ```tact
+ * contract Proxy {
+ *   to: Address;
+ *   init() {
+ *     // Warning: Insecure usage of zero address as default value
+ *     self.to = newAddress(0, 0);
+ *   }
+ *   fun setAddress(to: Address) {
+ *     self.to = to
+ *   }
+ * }
+ * ```
+ *
+ * Use instead:
+ * ```tact
+ * contract Proxy {
+ *   to: Address;
+ *   init(to: Address) {
+ *     // Fixed: Using the input value on initialization.
+ *     self.to = to;
+ *   }
+ *   fun setAddress(to: Address) {
+ *     self.to = to
+ *   }
+ * }
+ * ```
+ */
 export class ZeroAddress extends Detector {
-  /**
-   * A detector that identifies uses of the zero address.
-   *
-   * ## Why is it bad?
-   * Using the zero address in smart contracts is typically problematic because it can be
-   * exploited as a default or uninitialized address, leading to unintended transfers and
-   * security vulnerabilities. Additionally, operations involving the zero address can
-   * result in loss of funds or tokens, as there is no private key to access this address.
-   *
-   * ## Example
-   * ```tact
-   * contract Proxy {
-   *   to: Address;
-   *   init() {
-   *     // Warning: Insecure usage of zero address as default value
-   *     self.to = newAddress(0, 0);
-   *   }
-   *   fun setAddress(to: Address) {
-   *     self.to = to
-   *   }
-   * }
-   * ```
-   *
-   * Use instead:
-   * ```tact
-   * contract Proxy {
-   *   to: Address;
-   *   init(to: Address) {
-   *     // Fixed: Using the input value on initialization.
-   *     self.to = to;
-   *   }
-   *   fun setAddress(to: Address) {
-   *     self.to = to
-   *   }
-   * }
-   * ```
-   */
   check(cu: CompilationUnit): MistiTactError[] {
     return cu.ast.getProgramEntries().reduce((acc, node) => {
       return acc.concat(
