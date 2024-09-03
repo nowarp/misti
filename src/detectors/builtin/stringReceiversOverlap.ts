9a3e1b22f6bb5ccea124be2a088f710f72cc01db
Georgiy Komarov <jubnzv@gmail.com>
2024-09-03 13:49:17 +0000

    feat(stringReceiversOverlap): Taint analysis

--- src/detectors/builtin/stringReceiversOverlap.ts
+++ src/detectors/builtin/stringReceiversOverlap.ts
@@ -1,14 +1,151 @@
 import { Detector } from "../detector";
-import { CompilationUnit } from "../../internals/ir";
 import { MistiTactWarning, Severity } from "../../internals/warnings";
+import { CompilationUnit, BasicBlock, CFG } from "../../internals/ir";
 import {
   forEachExpression,
   forEachStatement,
 } from "../../internals/tactASTUtil";
+import { JoinSemilattice } from "../../internals/lattice";
+import { WorklistSolver } from "../../internals/solver/";
+import { Transfer } from "../../internals/transfer";
 import {
   AstExpression,
+  AstStatementLet,
+  AstStatement,
   AstReceiver,
 } from "@tact-lang/compiler/dist/grammar/ast";
+
+interface TaintState {
+  // Generic receiver's string argument name
+  readonly argName: string;
+  // String receiver names
+  readonly stringReceiverNames: Set<string>;
+  // Variables tainted with receiver's argument
+  argTaint: Set<string>;
+  // Variables tainted with string literal used in a string receiver
+  literalTaint: Set<string>;
+}
+
+/**
+ * A powerset lattice that keeps taint state in dataflow.
+ */
+class TaintLattice implements JoinSemilattice<TaintState> {
+  constructor(
+    private readonly argName: string,
+    private readonly stringReceiverNames: Set<string>,
+  ) {}
+
+  bottom(): TaintState {
+    return {
+      argName: this.argName,
+      stringReceiverNames: this.stringReceiverNames,
+      argTaint: new Set(),
+      literalTaint: new Set(),
+    };
+  }
+
+  join(a: TaintState, b: TaintState): TaintState {
+    const argTaint = new Set([...a.argTaint, ...b.argTaint]);
+    const literalTaint = new Set([...a.literalTaint, ...b.literalTaint]);
+    return {
+      argName: this.argName,
+      stringReceiverNames: this.stringReceiverNames,
+      argTaint,
+      literalTaint,
+    };
+  }
+
+  leq(a: TaintState, b: TaintState): boolean {
+    return (
+      [...a.argTaint].every((x) => b.argTaint.has(x)) &&
+      [...a.literalTaint].every((x) => b.literalTaint.has(x))
+    );
+  }
+}
+
+class StringReceiversOverlapTransfer implements Transfer<TaintState> {
+  public transfer(
+    inState: TaintState,
+    _node: BasicBlock,
+    stmt: AstStatement,
+  ): TaintState {
+    const outState = {
+      argName: inState.argName,
+      stringReceiverNames: inState.stringReceiverNames,
+      argTaint: inState.argTaint,
+      literalTaint: inState.literalTaint,
+    };
+    this.processStatements(outState, stmt);
+    return outState;
+  }
+
+  /**
+   * Processes taint information for untyped let statements in the following format:
+   * var newTaint = <id | literal | previousTaint>;
+   */
+  private processUntypedLet(outState: TaintState, stmt: AstStatementLet): void {
+    if (stmt.expression.kind === "id") {
+      const rhs = stmt.expression.text;
+      if (rhs === outState.argName || outState.argTaint.has(rhs)) {
+        // let newTaint = arg;
+        // let newTaint = previousArgTaint;
+        outState.argTaint.add(stmt.name.text);
+      } else if (outState.literalTaint.has(rhs)) {
+        // let newTaint = previousLiteralTaint;
+        outState.literalTaint.add(stmt.name.text);
+      }
+    } else if (
+      stmt.expression.kind === "string" &&
+      outState.stringReceiverNames.has(stmt.expression.value)
+    ) {
+      // let newTaint = "receiverName";
+      outState.literalTaint.add(stmt.name.text);
+    }
+  }
+
+  /**
+   * Processes taint information for typed let statements in the following format:
+   * let newTaint: String = <id | literal | previousTaint>;
+   */
+  private processTypedLet(outState: TaintState, stmt: AstStatementLet): void {
+    forEachExpression(stmt.expression, (expr) => {
+      if (expr.kind === "id") {
+        // Any operation involving receiver's argument or previous taints with it creates a new taint
+        if (
+          expr.text === outState.argName ||
+          outState.argTaint.has(expr.text)
+        ) {
+          outState.argTaint.add(stmt.name.text);
+        }
+        // Any operation involving string receiver name or previous taints with it creates a new taint
+        if (
+          outState.stringReceiverNames.has(expr.text) ||
+          outState.literalTaint.has(expr.text)
+        ) {
+          outState.literalTaint.add(stmt.name.text);
+        }
+      } else if (
+        expr.kind === "string" &&
+        outState.stringReceiverNames.has(expr.value)
+      ) {
+        // New taint with receiver name
+        outState.literalTaint.add(stmt.name.text);
+      }
+    });
+  }
+
+  private processStatements(outState: TaintState, stmt: AstStatement): void {
+    if (stmt.kind === "statement_let") {
+      if (stmt.type === null) {
+        // var newTaint = <id | literal | previousTaint>;
+        this.processUntypedLet(outState, stmt);
+      } else if (stmt.type.kind === "type_id" && stmt.type.text === "String") {
+        // let newTaint: String = <expr involving taints>;
+        this.processTypedLet(outState, stmt);
+      }
+    }
+  }
+}
 
 /**
  * A detector that finds overlapping messages between general string receivers and string receivers.
@@ -38,17 +175,48 @@ import {
 export class StringReceiversOverlap extends Detector {
   async check(cu: CompilationUnit): Promise<MistiTactWarning[]> {
     const stringReceivers = this.getStringReceiverNames(cu);
-    return Array.from(cu.ast.getFunctions()).reduce((warnings, node) => {
-      if (node.kind === "receiver") {
+    let warnings: MistiTactWarning[] = [];
+    cu.forEachCFG(cu.ast, (cfg: CFG) => {
+      const node = cu.ast.getFunction(cfg.id);
+      if (node !== undefined && node.kind === "receiver") {
         const arg = this.findGenericReceiverArg(node);
         if (arg !== undefined) {
-          return warnings.concat(
-            this.checkConditions(node, arg, stringReceivers),
+          // Solve the dataflow problem to find taints at each point of the program
+          const lattice = new TaintLattice(arg, stringReceivers);
+          const transfer = new StringReceiversOverlapTransfer();
+          const solver = new WorklistSolver(
+            cu,
+            cfg,
+            transfer,
+            lattice,
+            "forward",
           );
+          const taintResults = solver.solve();
+
+          // Inspect conditions in each statement with respect to its taint state.
+          cfg.forEachBasicBlock(cu.ast, (stmt, bb) => {
+            const state = taintResults.getState(bb.idx);
+            if (state === undefined) {
+              this.ctx.logger.warn(
+                `${this.id}: Cannot find BasicBlock #${bb.idx}`,
+              );
+              return;
+            }
+            warnings = warnings.concat(
+              this.checkConditions(
+                node,
+                arg,
+                stringReceivers,
+                state.literalTaint,
+                state.argTaint,
+              ),
+            );
+          });
+          return warnings;
         }
       }
-      return warnings;
-    }, [] as MistiTactWarning[]);
+    });
+    return warnings;
   }
 
   /**
@@ -60,18 +228,34 @@ export class StringReceiversOverlap extends Detector {
     receiver: AstReceiver,
     argName: string,
     stringReceivers: Set<string>,
+    literalTaints: Set<string>,
+    argTaints: Set<string>,
   ): MistiTactWarning[] {
     const warnings: MistiTactWarning[] = [];
     forEachStatement(receiver, (stmt) => {
       // Conditional statements
       if (stmt.kind === "statement_condition") {
-        this.checkCondition(warnings, stmt.condition, argName, stringReceivers);
+        this.checkCondition(
+          warnings,
+          stmt.condition,
+          argName,
+          stringReceivers,
+          literalTaints,
+          argTaints,
+        );
       }
     });
     forEachExpression(receiver, (expr) => {
       // Ternary conditions
       if (expr.kind === "conditional") {
-        this.checkCondition(warnings, expr.condition, argName, stringReceivers);
+        this.checkCondition(
+          warnings,
+          expr.condition,
+          argName,
+          stringReceivers,
+          literalTaints,
+          argTaints,
+        );
       }
     });
     return warnings;
@@ -86,11 +270,14 @@ export class StringReceiversOverlap extends Detector {
     condition: AstExpression,
     argName: string,
     stringReceivers: Set<string>,
+    literalTaints: Set<string>,
+    argTaints: Set<string>,
   ): void {
     const isArg = (expr: AstExpression) =>
-      expr.kind === "id" && expr.text === argName;
+      expr.kind === "id" && (expr.text === argName || argTaints.has(expr.text));
     const isOverlappingStringLiteral = (expr: AstExpression) =>
-      expr.kind === "string" && stringReceivers.has(expr.value);
+      (expr.kind === "string" && stringReceivers.has(expr.value)) ||
+      (expr.kind === "id" && literalTaints.has(expr.text));
     const isOverlappingComparison = (lhs: AstExpression, rhs: AstExpression) =>
       isArg(lhs) && isOverlappingStringLiteral(rhs);
     // Iterate recursively to find cases like `(msg === "overlap") && whatever_else`
