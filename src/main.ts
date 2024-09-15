26d8e92f3b56dd346c4b6722176be6e22800e0a3
Georgiy Komarov <jubnzv@gmail.com>
2024-09-15 01:32:54 +0000

    feat(main): Handle errors thrown within `cli.ts`

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { report, runMistiCommand } from "./cli";
 
 async function main() {
   const args = process.argv.slice(2);
-  const result = await runMistiCommand(args);
-  report(result);
+  try {
+    const result = await runMistiCommand(args);
+    report(result);
+  } catch (error: any) {
+    console.error(error.message);
+    process.exit(1);
+  }
 }
 
 main();
