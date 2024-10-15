330fc8b06ca6ff46a51a559ca76b5513f23385bb
Esorat <95045543+Esorat@users.noreply.github.com>
2024-10-15 13:21:36 +0700

    feat(sendInLoop): Add docstrings (#176)
    * fix: add docstrings
    
    * chore: fix spell
    
    * fix: remove 'good' example and simplify docstring.

--- src/detectors/builtin/sendInLoop.ts
+++ src/detectors/builtin/sendInLoop.ts
@@ -8,6 +8,31 @@ import {
   idText,
 } from "@tact-lang/compiler/dist/grammar/ast";
 
+/**
+ * An optional detector that identifies send functions being called inside loops.
+ *
+ * ## Why is it bad?
+ * Calling send functions inside loops can lead to unintended consequences, such as
+ * excessive message sending, increased gas consumption, and potential race conditions.
+ * Loops with send calls should be refactored to avoid these issues. This detector helps
+ * flag such code, prompting the developer to reconsider the design.
+ *
+ * ## Example
+ * ```tact
+ * fun exampleWhileLoop(limit: Int, owner: Address) {
+ *   let i = 0;
+ *   while (i < limit) {
+ *       send(SendParameters{ // Highlighted: An auditor should review the loop
+ *           to: owner,
+ *           value: 0,
+ *           bounce: false,
+ *           body: Msg{ a: i }.toCell()
+ *       });
+ *       i += 1;
+ *   }
+ * }
+ * ```
+ */
 export class SendInLoop extends ASTDetector {
   severity = Severity.MEDIUM;
 
