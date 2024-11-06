95359b8ac1362dccc4c8e8c7469ab7421afd54fb
Georgiy Komarov <jubnzv@gmail.com>
2024-11-06 12:14:21 -0500

    Update src/internals/ir/callGraph.ts

--- src/internals/ir/callGraph.ts
+++ src/internals/ir/callGraph.ts
@@ -183,7 +183,7 @@ export class CallGraph {
   /**
    * Processes an expression, identifying static and method calls to add edges.
    * @param expr The AST expression to process.
-   * @param callerId - The ID of the calling node.
+   * @param callerId The ID of the calling node.
    */
   private processExpression(expr: AstExpression, callerId: CGNodeId) {
     if (expr.kind === "static_call") {
