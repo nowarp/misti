33fe99dbf24c0377b5904af7cb52a389db61d242
Georgiy Komarov <jubnzv@gmail.com>
2024-11-20 06:40:27 +0000

    feat(scripts): Rewrite genVersions to CommonJS
    This is required to run it with `node` in `postinstall`.
    
    Otherwise, we have either to install all the dependencies or add
    `ts-node` as a dependency (not dev depenendency).

--- scripts/genVersions.cjs
+++ scripts/genVersions.cjs
@@ -1,27 +1,25 @@
 /**
  * Generates Misti and Tact versions in `src/version-info.ts`.
  *
- * This is needed to avoid reading them from `package.json` at runtime to simplify
- * building the distribution package.
+ * This is needed to avoid reading them from `package.json` at runtime to
+ * simplify building the distribution package.
  *
- * **Note:** If the `MISTI_RELEASE` environment variable is set to `'1'`, the Git
- * revision will not be included in the version string.
- *
- * @packageDocumentation
+ * **Note:** If the `MISTI_RELEASE` environment variable is set to `'1'`, the
+ * Git revision will not be included in the version string.
  */
 
-import * as fs from "fs";
-import * as path from "path";
-import { execSync } from "child_process";
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
 const packageJsonPath = path.join(__dirname, "..", "package.json");
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
-const packageVersion: string = packageJson.version;
+const packageVersion = packageJson.version;
 
 // Initialize MISTI_VERSION with packageVersion
-let MISTI_VERSION: string = packageVersion;
+let MISTI_VERSION = packageVersion;
 
-if (process.env.MISTI_RELEASE !== '1') {
+if (process.env.MISTI_RELEASE !== "1") {
   // Retrieve the Git revision number
   let gitRevision = "unknown";
   try {
@@ -34,13 +32,11 @@ if (process.env.MISTI_RELEASE !== '1') {
   MISTI_VERSION = `${packageVersion}-${gitRevision}`;
 }
 
-const TACT_COMPILER_VERSION: string =
-  packageJson.dependencies["@tact-lang/compiler"];
+const TACT_COMPILER_VERSION = packageJson.dependencies["@tact-lang/compiler"];
 
-const normalizeVersion = (version: string): string =>
-  version.startsWith("^") || version.startsWith("~")
-    ? version.slice(1)
-    : version;
+const normalizeVersion = (version) =>
+    version.startsWith("^") || version.startsWith("~") ? version.slice(1)
+                                                       : version;
 
 const content = `
 export const MISTI_VERSION = '${MISTI_VERSION}';
