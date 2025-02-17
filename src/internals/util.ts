81884dd393f8be39e1b2e6de1e1ca79bb9b76a61
Georgiy Komarov <jubnzv@gmail.com>
2025-02-17 07:35:02 -0500

    fix(cellBounds): Infinite recursion due to incorrect `leq` impl (#272)
    Closes #270

--- src/internals/util.ts
+++ src/internals/util.ts
@@ -9,21 +9,32 @@ import path from "path";
 
 export const mergeSets = <T>(lhs: Set<T>, rhs: Set<T>): Set<T> =>
   new Set([...lhs, ...rhs]);
-export const isSetSubsetOf = <T>(lhs: Set<T>, rhs: Set<T>): boolean =>
-  [...lhs].every((elem) => rhs.has(elem));
+export const isSetSubsetOf = <T>(
+  lhs: Set<T>,
+  rhs: Set<T>,
+  eq: (a: T, b: T) => boolean = (a, b) => a === b,
+): boolean =>
+  [...lhs].every((elem) => [...rhs].some((rElem) => eq(elem, rElem)));
 export const intersectSets = <T>(setA: Set<T>, setB: Set<T>): Set<T> =>
   new Set([...setA].filter((item) => setB.has(item)));
 
 export const mergeLists = <T>(lhs: T[], rhs: T[]): T[] => [...lhs, ...rhs];
-export const isListSubsetOf = <T>(lhs: T[], rhs: T[]): boolean =>
-  lhs.every((elem) => rhs.includes(elem));
+export const isListSubsetOf = <T>(
+  lhs: T[],
+  rhs: T[],
+  eq: (a: T, b: T) => boolean = (a, b) => a === b,
+): boolean => lhs.every((elem) => rhs.some((rElem) => eq(elem, rElem)));
 export const intersectLists = <T>(l1: T[], l2: T[]): T[] =>
   l1.filter((element) => l2.includes(element));
 
 export const mergeMaps = <K, V>(lhs: Map<K, V>, rhs: Map<K, V>): Map<K, V> =>
   new Map([...lhs, ...rhs]);
-export const isMapSubsetOf = <K, V>(lhs: Map<K, V>, rhs: Map<K, V>): boolean =>
-  [...lhs].every(([key, value]) => rhs.has(key) && rhs.get(key) === value);
+export const isMapSubsetOf = <K, V>(
+  lhs: Map<K, V>,
+  rhs: Map<K, V>,
+  eq: (a: V, b: V) => boolean = (a, b) => a === b,
+): boolean =>
+  [...lhs].every(([key, value]) => rhs.has(key) && eq(value, rhs.get(key)!));
 export const intersectMaps = <K, V>(
   mapA: Map<K, V>,
   mapB: Map<K, V>,
