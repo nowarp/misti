49ed0357198f956c9db338edeeb5f11365007477
Georgiy Komarov <jubnzv@gmail.com>
2025-03-03 07:38:21 +0000

    chore(test): A bit less intrusive hack for `maxWorkers`

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset : "ts-jest",
   testEnvironment : "node",
   testPathIgnorePatterns : [ "/node_modules/", "/dist/" ],
-  maxWorkers: 3,
+  maxWorkers: "30%",
   testTimeout: 15000,
 };
