c3b7aa078348c4d6dbd35e3e4f6e6eaa85cbe90b
Georgiy Komarov <jubnzv@gmail.com>
2025-03-03 07:07:48 +0000

    fix(test): Workaround for #315 to reduce OOM suffering

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset : "ts-jest",
   testEnvironment : "node",
   testPathIgnorePatterns : [ "/node_modules/", "/dist/" ],
-  maxWorkers: "50%",
+  maxWorkers: 3,
   testTimeout: 15000,
 };
