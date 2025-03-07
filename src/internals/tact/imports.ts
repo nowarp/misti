7b71f05bbebc46107e8eb708a00f449cc2edc417
Georgiy Komarov <jubnzv@gmail.com>
2025-03-07 05:55:44 +0000

    feat(imports): Use `@tact-lang/compiler`
    Closes #321

--- src/internals/tact/imports.ts
+++ src/internals/tact/imports.ts
@@ -43,41 +43,30 @@ export {
   AstStatementExpression,
   AstLiteral,
   AstFunctionAttributeName,
-} from "@tact-lang/compiler/dist/ast/ast";
-export { prettyPrint } from "@tact-lang/compiler/dist/ast/ast-printer";
-export {
+  prettyPrint,
   idText,
   isSelfId,
   tryExtractPath,
   isLiteral,
   getAstFactory,
   eqExpressions,
-} from "@tact-lang/compiler/dist/ast/ast-helpers";
-export { getAstUtil } from "@tact-lang/compiler/dist/ast/util";
-
-export { ItemOrigin, Source } from "@tact-lang/compiler/dist/imports/source";
-export { asString as importAsString } from "@tact-lang/compiler/dist/imports/path";
-
-export {
+  getAstUtil,
+  ItemOrigin,
+  Source,
+  asString as importAsString,
   SrcInfo,
   dummySrcInfo,
   getParser,
-} from "@tact-lang/compiler/dist/grammar";
-export { srcInfoEqual } from "@tact-lang/compiler/dist/grammar/src-info";
-
-export { precompile } from "@tact-lang/compiler/dist/pipeline/precompile";
-export { enableFeatures } from "@tact-lang/compiler/dist/pipeline/build";
-
-export { default as stdLibFiles } from "@tact-lang/compiler/dist/stdlib/stdlib";
-
-export { AstStore, getRawAST } from "@tact-lang/compiler/dist/context/store";
-export { CompilerContext } from "@tact-lang/compiler/dist/context/context";
-
-export {
+  srcInfoEqual,
+  precompile,
+  enableFeatures,
+  stdLibFiles,
+  AstStore,
+  getRawAST,
+  CompilerContext,
   Config,
   Project,
   parseConfig,
-} from "@tact-lang/compiler/dist/config/parseConfig";
-
-export { evalConstantExpression } from "@tact-lang/compiler/dist/optimizer/constEval";
-export { ensureInt } from "@tact-lang/compiler/dist/optimizer/interpreter";
+  evalConstantExpression,
+  ensureInt,
+} from "@tact-lang/compiler";
